35f8383960c73132a2ed11bae0cf7f5d998aceb4
Alberto Morales <amorales@amorales-pb-cap.local>
2016-06-22 11:12:38 -0400

    adding the enumerable for all items and folders

--- main.rb
+++ main.rb
@@ -25,16 +25,66 @@ class Folder < OpenStruct
     accummulator
   end
 
-  # using enumerable
+  ########## amorales preferred methods ######
+  def all_child_folders
+    AllChildFolders.new(self)
+  end
+
+  def all_items
+    AllItems.new(self)
+  end
+  ############################################
+end
+
+# the class to simulate an item
+class Item < OpenStruct
+  def self.get(id)
+    Folder.get(id)
+  end
+end
+
+# the enumerator for child folders
+class AllChildFolders
   include Enumerable
+
+  attr_reader :source
+
+  def initialize(source)
+    @source = source
+  end
+
   def each(&block)
-    children.each do |child_id|
+    source.children.each do |child_id|
       child = Folder.get(child_id)
       yield child
-      child.each(&block)
+      child.all_child_folders.each(&block)
     end
   end
-  # probably better called each child
-  alias each_child each
+end
 
+# the enumerator for items
+class AllItems
+  include Enumerable
+
+  attr_reader :source
+
+  def initialize(source)
+    @source = source
+  end
+
+  def each(&block)
+    # iterate over the items in the folder
+    source.items.each do |item_id|
+      item = Item.get(item_id)
+      yield item
+    end
+    # iterate over the other folders
+    # and iterate over the items for those folders
+    source.all_child_folders.each do |folder|
+      folder.items.each do |item_id|
+        item = Item.get(item_id)
+        yield item
+      end
+    end
+  end
 end
--- main_spec.rb
+++ main_spec.rb
@@ -5,15 +5,18 @@ class Folder < OpenStruct
   # the container for tests
   def self.db
     @db ||= [
-      Folder.new({id: 'a', children: %w(b c d e)}),
-      Folder.new({id: 'b', children: %w(f g h)}),
-      Folder.new({id: 'c', children: []}),
-      Folder.new({id: 'd', children: []}),
-      Folder.new({id: 'e', children: []}),
-      Folder.new({id: 'f', children: []}),
-      Folder.new({id: 'g', children: []}),
-      Folder.new({id: 'h', children: %w(i)}),
-      Folder.new({id: 'i', children: []}),
+      Folder.new({id: 'a', children: %w(b c d e), items: []}),
+      Folder.new({id: 'b', children: %w(f g h), items: []}),
+      Folder.new({id: 'c', children: [], items: %w(j)}),
+      Folder.new({id: 'd', children: [], items: []}),
+      Folder.new({id: 'e', children: [], items: []}),
+      Folder.new({id: 'f', children: [], items: []}),
+      Folder.new({id: 'g', children: [], items: []}),
+      Folder.new({id: 'h', children: %w(i), items: %w(k l)}),
+      Folder.new({id: 'i', children: [], items: []}),
+      Item.new({id: 'j'}),
+      Item.new({id: 'k'}),
+      Item.new({id: 'l'}),
     ]
   end
 
@@ -23,41 +26,51 @@ class Folder < OpenStruct
   end
 end
 
+
 describe 'get' do
+  let(:folder) { Folder.get('a') }
+
   it 'should find a folder by id' do
     expect(Folder.get('a')).not_to be_nil
     expect(Folder.get('a').id).to eq('a')
   end
 
   it 'should find all children via class method' do
-    folder = Folder.get('a')
     expect(Folder.all_children(folder).size).to eq(8)
     expect(Folder.all_children(folder).map(&:id).sort).to eq(%w(b c d e f g h i))
   end
 
   it 'should find all children via instance method' do
-    folder = Folder.get('a')
     expect(folder.all_children.size).to eq(8)
     expect(folder.all_children.map(&:id).sort).to eq(%w(b c d e f g h i))
   end
 
-  it 'should find all children via each method' do
-    folder = Folder.get('a')
+  it 'should find all children via all_child_folders.each method' do
     all = []
-    folder.each_child do |child|
+    folder.all_child_folders.each do |child|
       all << child
     end
     expect(all.size).to eq(8)
-    expect(folder.all_children.map(&:id).sort).to eq(%w(b c d e f g h i))
   end
+
+  it 'should create a map if that is what folks want' do
+    array = folder.all_child_folders.map(&:id).sort
+    expect(array).to eq(%w(b c d e f g h i))
+  end
+
 
   it 'should be fine with no children, (not blow up)' do
     folder = Folder.get('i')
     all = []
-    folder.each_child do |child|
+    folder.all_child_folders.each do |child|
       all << child
     end
     expect(all.size).to eq(0)
+  end
+
+  it 'should find all the items for the folder' do
+    expect(folder.all_items.map(&:id).size).to eq(3)
+    expect(folder.all_items.map(&:id).sort).to eq(%w(j k l))
   end
 end
 
